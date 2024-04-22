49f2e72435d5a17bc84b5761eb65b5f9e93c8127
Job Hernandez <hj93@protonmail.com>
2024-04-21 22:26:47 -0700

    refactor

--- src/lisp_is_math/core.clj
+++ src/lisp_is_math/core.clj
@@ -1,72 +1,72 @@
 (ns lisp-is-math.core
   (:require [clojure.math :as math]))
 
-(defmacro define-element-wise [function fn-name tensor tensor2]
-  `(defn ~(symbol (str fn-name)) [~tensor ~tensor2]
-     (cond (number? ~tensor)
-           (map (fn [x#] (~function x# ~tensor)) ~tensor2)
-           (number? ~tensor2)
-           (map (fn [x#] (~function x# ~tensor2)) ~tensor)
+(defmacro define-element-wise [function fn-name]
+  `(defn ~(symbol (str fn-name)) [tensor# tensor2#]
+     (cond (number? tensor#)
+           (map (fn [x#] (~function x# tensor#)) tensor2#)
+           (number? tensor2#)
+           (map (fn [x#] (~function x# tensor2#)) tensor#)
            :else
-           (map ~function ~tensor ~tensor2))))
+           (map ~function tensor# tensor2#))))
 
-(defmacro define-element-wise-one-param [function function-name t]
-  `(defn ~(symbol (str function-name)) [~t]
-     (map (fn [x#] (~function x#)) ~t)))
+(defmacro define-element-wise-one-param [function function-name]
+  `(defn ~(symbol (str function-name)) [t#]
+     (map (fn [x#] (~function x#)) t#)))
 
 ;;; vector arithmetic
 
-(define-element-wise + add-vectors v v2)
-(define-element-wise - sub-vectors v v2)
+(define-element-wise + add-v)
+(define-element-wise - sub-v)
 
 ;; dot product
 
-(define-element-wise * mul-vectors v v2)
+(define-element-wise * mul-v)
 
 (defn dot-product [v v2]
-  (reduce + (mul-vectors v v2)))
+  (reduce + (mul-v v v2)))
 
-;; matrix by scalar
+;; matrix  by scalar
 
-(define-element-wise mul-vectors mul-matrix m1 m2)
+(define-element-wise mul-v mul-m)
 
 ;; matrix arithmetic
 
-(define-element-wise add-vectors add-matrices m m2)
-(define-element-wise sub-vectors  sub-matrices m m2)
+(define-element-wise add-v add-m)
+(define-element-wise sub-v  sub-m)
 
 ;; power
 
-(define-element-wise math/pow pow-vectors v v2)
-(define-element-wise pow-vectors pow-matrices m m2)
+(define-element-wise math/pow pow-v)
+(define-element-wise pow-v pow-m)
 
 ;; max
 
-(define-element-wise max max-vectors v v2)
-(define-element-wise max-vectors max-matrices m m2)
+(define-element-wise max max-v)
+(define-element-wise max-v max-m)
 
 ;; min
 
-(define-element-wise min min-vectors v v2)
-(define-element-wise min-vectors min-matrix m1 m2)
+(define-element-wise min min-v)
+(define-element-wise min-v min-m)
 
 ;; exp
 
-(define-element-wise-one-param math/exp exp-vectors t)
-(define-element-wise-one-param exp-vectors exp-matrice t)
+(define-element-wise-one-param math/exp exp-v)
+(define-element-wise-one-param exp-v exp-m)
 
 ;; log
 
-(define-element-wise-one-param math/log log-vector t)
-(define-element-wise-one-param log-vector log-matrix t)
+(define-element-wise-one-param math/log log-v)
+(define-element-wise-one-param log-v log-m)
 
 ;; abs
 
-(define-element-wise-one-param abs abs-vector t)
-(define-element-wise-one-param abs-vector abs-matrix t)
+(define-element-wise-one-param abs abs-v)
+(define-element-wise-one-param abs-v abs-m)
 
 ;; log10
 
-(define-element-wise-one-param  math/log10 log10-vector t)
-(define-element-wise-one-param log10-vector log10-matrix t)
+(define-element-wise-one-param  math/log10 log10-v)
+(define-element-wise-one-param log10-v log10-m)
 
